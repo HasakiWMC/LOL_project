d3aa5f32e4caa70c0935b982803184749af06d0b
wmc911 <wangmingcheng2016@gmail.com>
2018-12-08 20:49:22 +0800

    后台加入来自哪个服务器的判断

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ s.keep_alive = False  # 关闭多余连接
 
 app = Flask(__name__)
 
+api_key = "RGAPI-49d0df94-95b4-40fe-9932-f346d23486dd"
+
 
 @app.route('/')
 def hello_world():
@@ -15,10 +17,17 @@ def hello_world():
 
 @app.route('/searchSummoner', methods=['GET'])
 def search_summoner():
+    region_dict = {
+        "Korea": "kr",
+        "Europe West": "euw1"
+
+    }
     summoner_name = request.args.get("summonerName")
-    api_key = "RGAPI-49d0df94-95b4-40fe-9932-f346d23486dd"
+    region = request.args.get("region")
+    region_value = region_dict[region]
     response = s.get(
-        url=("https://kr.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s" % (summoner_name, api_key)))
+        url=("https://%s.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s" % (
+        region_value, summoner_name, api_key)))
     return response.text
 
 
