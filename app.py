93225879a2d50b38ea1ee799af37f8683f2ae3ee
wmc911 <wangmingcheng2016@gmail.com>
2018-12-09 16:18:42 +0800

    校检输入召唤师名字

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+from util.validate import Validate
 
 requests.adapters.DEFAULT_RETRIES = 5  # 增加重连次数
 s = requests.session()
@@ -7,7 +8,7 @@ s.keep_alive = False  # 关闭多余连接
 
 app = Flask(__name__)
 
-api_key = "RGAPI-49d0df94-95b4-40fe-9932-f346d23486dd"
+api_key = "RGAPI-d315d1b1-381d-4a03-b19c-b7047e020e3f"
 
 
 @app.route('/')
@@ -17,19 +18,23 @@ def hello_world():
 
 @app.route('/searchSummoner', methods=['GET'])
 def search_summoner():
+    region = request.args.get("region")
+    summoner_name = request.args.get("summonerName")
+
+    # 校检region和summoner名字是否规范
+    # Validate.validate_summoner_input(region, summoner_name)
+
     region_dict = {
         "Korea": "kr",
         "Europe West": "euw1"
 
     }
-    summoner_name = request.args.get("summonerName")
-    region = request.args.get("region")
     region_value = region_dict[region]
+
+
     response = s.get(
         url=("https://%s.api.riotgames.com/lol/summoner/v3/summoners/by-name/%s?api_key=%s" % (
         region_value, summoner_name, api_key)))
-    import time
-    time.sleep(1)
     return response.text
 
 
