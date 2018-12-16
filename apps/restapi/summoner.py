885a20faef7f80c9b50450eed588d9ff1e87bd7b
wmc3721 <512111127@qq.com>
2018-12-16 22:28:03 +0800

    将获取到的数据归类整理放入result中，方便前台读取

--- apps/restapi/summoner.py
+++ apps/restapi/summoner.py
@@ -14,7 +14,6 @@ s.keep_alive = False  # 关闭多余连接
 class SearchSummonerAPI(MethodView):
 
     def get(self):
-        result_data = ""
         try:
             region = request.args.get("region")
             summoner_name = request.args.get("summonerName")
@@ -24,6 +23,7 @@ class SearchSummonerAPI(MethodView):
 
             region_value = Constant.REGIONAL_ENDPOINTS[region]
 
+            # todo 将此内容移到service中 ===start===
             api_value = RoitAPI.API_GET_SUMMONER_BY_NAME.format(summonerName=summoner_name)
 
             request_url = "https://%s.api.riotgames.com%s?api_key=%s" % (region_value, api_value, Constant.API_KEY)
@@ -31,7 +31,26 @@ class SearchSummonerAPI(MethodView):
             print("request url = %s" % request_url)
 
             response = s.get(url=request_url)
-            result_data = response.text
+
+            response_data = json.loads(response.text)
+
+            # 用json.loads将str转为dict
+            # print(type(response.text))  # <class 'str'>
+            # print(type(response_data))  # <class 'dict'>
+
+            summoner_header = {
+                "name": response_data.get('name'),
+                "profileIconId": response_data.get('profileIconId'),
+                "summonerLevel": response_data.get('summonerLevel')
+            }
+
+            # SummonerDetail页面有很多数据，需要将每个模块的数据单独封装，便于前台读取
+            result = {
+                "summoner_header": summoner_header
+            }
+
+            # todo 将此内容移到service中 ===end===
+
         except LOLException as ex:
             print("错误码 = %s，错误描述 = %s" % (ex.get_err_code(), ex.get_err_desc()))
             if ex.get_err_code() == 1000:
@@ -40,5 +59,5 @@ class SearchSummonerAPI(MethodView):
         except Exception as ex:
             print(ex)
             return jsonify({"retCode": RetCode.RET_CODE_SYSTEM_ERROR})
-        print("返回数据 = %s" % result_data)
-        return jsonify({"retCode": 0, "data": json.loads(result_data)})
+        print("返回数据 = %s" % result)
+        return jsonify({"retCode": 0, "data": result})
