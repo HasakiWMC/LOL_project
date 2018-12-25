4293bf6462853bd38abd9c91c03304211ab3eaa3
ubuntuRoot <512111127@qq.com>
2018-12-25 09:16:20 -0800

    因为软件包放入了lib下，打包脚本相应修改

--- CI/productPackage/build.sh
+++ CI/productPackage/build.sh
@@ -6,20 +6,20 @@ PROJECT_HOME=${CURRENT_PATH}/../../
 
 RELEASE_PATH=${PROJECT_HOME}/release
 
-if [[ ! -f ${RELEASE_PATH}/release_software.tar.gz ]];then
+LIB_PATH=${PROJECT_HOME}/lib
+
+if [[ ! -f ${LIB_PATH}/release_software.tar.gz ]];then
 {
-    echo "release_software.tar.gz not exist!"
+    echo "${LIB_PATH}/release_software.tar.gz not exist!"
     exit 100
 }
 fi
 
+rm -rf ${RELEASE_PATH}/*
+
 cd ${RELEASE_PATH}
 
-rm -rf LOL/
-rm -rf install.sh
-rm -rf LOL_WEB.tar.gz
-
-tar -zxvf release_software.tar.gz -C ./ > /dev/null
+tar -zxvf ${LIB_PATH}/release_software.tar.gz -C ./ > /dev/null
 
 bash ${CURRENT_PATH}/build_web.sh
 
