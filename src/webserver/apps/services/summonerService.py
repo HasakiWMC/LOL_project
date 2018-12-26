0edd65de63200e874a449251844aaabf3805cc22
wmc3721 <512111127@qq.com>
2018-12-27 01:16:50 +0800

    查询排位信息根据灵活组排和单双排放入字典中，如无传空字典

--- src/webserver/apps/services/summonerService.py
+++ src/webserver/apps/services/summonerService.py
@@ -6,7 +6,7 @@ from flask import json
 from apps.common.exception import LOLException
 from apps.common.retcode import RetCode
 from apps.common.validate_util import ValidateUtils
-from apps.common.const import Constant, RoitAPI
+from apps.common.const import Constant, RiotAPI
 from apps.common.private_const import PrivateConstant
 
 # todo 这里的s是进来的所有用户都用同一个？怎么高并发？
@@ -23,7 +23,14 @@ class SummonerService:
         ValidateUtils.validate_summoner_input(region, summoner_name)
         region_value = Constant.REGIONAL_ENDPOINTS[region]
         # 获取召唤师基本信息，其中id需要被后续请求用到
-        api_value = RoitAPI.API_V4_GET_SUMMONER_BY_NAME.format(summonerName=summoner_name)
+        api_value = RiotAPI.API_V4_GET_SUMMONER_BY_NAME.format(summonerName=summoner_name)
+        # todo 工具类中加个方法同一个ip判断api请求是否超过限制，如超过，设置五分钟内禁止访问；
+        # todo 同时整个web也要全局设置变量，如果超过，显示给用户系统繁忙
+        """
+        Rate Limits：
+        20 requests every 1 seconds
+        100 requests every 2 minutes 
+        """
         req_url = "https://%s.api.riotgames.com%s?api_key=%s" % (region_value, api_value, PrivateConstant.API_KEY)
         print("request url = %s" % req_url)
         res = s.get(url=req_url)
@@ -44,22 +51,37 @@ class SummonerService:
         }
         summoner_id = response_data.get('id')
         # 根据id获取召唤师联盟位置信息
-        api_value = RoitAPI.API_V4_GET_LEAGUE_POSITIONS_BY_ID.format(encryptedSummonerId=summoner_id)
+        api_value = RiotAPI.API_V4_GET_LEAGUE_POSITIONS_BY_ID.format(encryptedSummonerId=summoner_id)
         req_url = "https://%s.api.riotgames.com%s?api_key=%s" % (region_value, api_value, PrivateConstant.API_KEY)
         print("request url = %s" % req_url)
         res = s.get(url=req_url)
         # 用json.loads将str转为dict
         response_data = json.loads(res.text)
         # todo 查段位信息不止召唤师峡谷，先取第0个，后续要考虑其他排位形式
-        summoner_tier = {
-            "queueType": response_data[0].get('queueType'),
-            "wins": response_data[0].get('wins'),
-            "losses": response_data[0].get('losses'),
-            "leagueName": response_data[0].get('leagueName'),
-            "rank": response_data[0].get('rank'),
-            "tier": response_data[0].get('tier'),
-            "leaguePoints": response_data[0].get('leaguePoints')
-        }
+
+        if len(response_data) > 0:
+            summoner_tier = {}
+            for position_item in response_data:
+                queue_type = position_item.get("queueType")
+                wins = position_item.get("wins")
+                losses = position_item.get("losses")
+                league_name = position_item.get("leagueName")
+                rank = position_item.get("rank")
+                tier = position_item.get("tier")
+                league_points = position_item.get("leaguePoints")
+                summoner_tier_item = {
+                    "queueType": queue_type,
+                    "wins": wins,
+                    "losses": losses,
+                    "leagueName": league_name,
+                    "rank": rank,
+                    "tier": tier,
+                    "leaguePoints": league_points,
+                }
+                summoner_tier.update({queue_type: summoner_tier_item})
+        else:
+            summoner_tier = {}
+
         # SummonerDetail页面有很多数据，需要将每个模块的数据单独封装，便于前台读取
         result = {
             "summoner_profile": summoner_profile,
