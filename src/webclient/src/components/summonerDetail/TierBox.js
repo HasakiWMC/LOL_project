b30db645a51e99b1895dcb062320bce3c2bb6b10
wmc3721 <512111127@qq.com>
2018-12-27 01:20:13 +0800

    1.排位数据接口变更修改 2.单双排显示图标写活 3.段位名称首字母大写 4.不是王者宗师大师就显示小段位

--- src/webclient/src/components/summonerDetail/TierBox.js
+++ src/webclient/src/components/summonerDetail/TierBox.js
@@ -1,33 +1,92 @@
+/* eslint-disable max-len */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import '../../css/Summoner.css'
 import {PropTypes} from "prop-types";
 
+const rankRoman2Int = {
+    "I": 1,
+    "II": 2,
+    "III": 3,
+    "IV": 4,
+    "V": 5
+};
+
+//tier从全大写转换为首字母大写，其余小写
+const changeTierSpell = (tier) => {
+    return tier.toLowerCase().replace(/(^)[a-z]/g, (char) => char.toUpperCase());
+};
 
 class TierBox extends Component {
     state = {
-        queueType: "",
-        wins: 0,
-        losses: 0,
-        leagueName: "",
-        rank: "",
-        tier: "",
-        leaguePoints: 0,
+        RANKED_SOLO_5x5: {
+            queueType: "",
+            wins: 0,
+            losses: 0,
+            leagueName: "",
+            rank: "",
+            tier: "",
+            leaguePoints: 0,
+        },
+        RANKED_FLEX_SR: {
+            queueType: "",
+            wins: 0,
+            losses: 0,
+            leagueName: "",
+            rank: "",
+            tier: "",
+            leaguePoints: 0,
+        }
+
     };
 
     componentDidMount() {
         if (this.props.tier) {
             console.log("TierBox接收数据：", this.props.tier);
-            this.setState({
-                queueType: this.props.tier.queueType,
-                wins: this.props.tier.wins,
-                losses: this.props.tier.losses,
-                leagueName: this.props.tier.leagueName,
-                rank: this.props.tier.rank,
-                tier: this.props.tier.tier,
-                leaguePoints: this.props.tier.leaguePoints,
-            });
+
+            if (this.props.tier.hasOwnProperty("RANKED_SOLO_5x5")) {
+                this.setState({
+                        RANKED_SOLO_5x5: {
+                            queueType: this.props.tier["RANKED_SOLO_5x5"]["queueType"],
+                            wins: this.props.tier["RANKED_SOLO_5x5"]["wins"],
+                            losses: this.props.tier["RANKED_SOLO_5x5"]["losses"],
+                            leagueName: this.props.tier["RANKED_SOLO_5x5"]["leagueName"],
+                            rank: this.props.tier["RANKED_SOLO_5x5"]["rank"],
+                            tier: this.props.tier["RANKED_SOLO_5x5"]["tier"],
+                            leaguePoints: this.props.tier["RANKED_SOLO_5x5"]["leaguePoints"],
+                        }
+                    }
+                )
+            } else {
+                this.setState({
+                        RANKED_SOLO_5x5: {}
+                    }
+                )
+            }
+            if (this.props.tier.hasOwnProperty("RANKED_FLEX_SR")) {
+                this.setState({
+                        RANKED_FLEX_SR: {
+                            queueType: this.props.tier["RANKED_FLEX_SR"]["queueType"],
+                            wins: this.props.tier["RANKED_FLEX_SR"]["wins"],
+                            losses: this.props.tier["RANKED_FLEX_SR"]["losses"],
+                            leagueName: this.props.tier["RANKED_FLEX_SR"]["leagueName"],
+                            rank: this.props.tier["RANKED_FLEX_SR"]["rank"],
+                            tier: this.props.tier["RANKED_FLEX_SR"]["tier"],
+                            leaguePoints: this.props.tier["RANKED_FLEX_SR"]["leaguePoints"],
+                        }
+                    }
+                )
+            } else {
+                this.setState({
+                        RANKED_FLEX_SR: {}
+                    }
+                )
+            }
+
+            setTimeout(() => {
+                console.log(this.state)
+            }, 1000);
         }
     }
 
@@ -35,34 +94,50 @@ class TierBox extends Component {
         // todo 是否需要搞国际化，影响后续写法
         // todo 图标路径需要拼接，前提需要知道所有的链接地址，以及unranked的情况
         // todo 段位名称需要国际化吗？中文英文应该都可以，如果只需要英文根据"tier"做首字母大写即可
-        const winRate = parseInt(100 * this.state.wins / (this.state.wins + this.state.losses));
+        const winSoloRate =
+            parseInt(100 * this.state.RANKED_SOLO_5x5.wins / (this.state.RANKED_SOLO_5x5.wins + this.state.RANKED_SOLO_5x5.losses));
 
+        // todo 需要判断是否存在段位，并且无段位需要显示unranked图标，其他信息都不显示
+        const tierImg = this.state.RANKED_SOLO_5x5.tier.toLowerCase() + "_" + rankRoman2Int[this.state.RANKED_SOLO_5x5.rank];
+
+        const tierImgSrc = "//opgg-static.akamaized.net/images/medals/" + tierImg + ".png";
+
+        // todo 继续完善灵活组排信息
         return (
+
             <div>
                 <div className="list-group-item">
                     <div className="SummonerRatingMedium">
                         <div className="Medal tip" title="单人排位">
-                            <img src="//opgg-static.akamaized.net/images/medals/grandmaster_1.png" className="Image"/>
+                            <img src={tierImgSrc} className="Image"/>
                         </div>
                         <div className="TierRankInfo">
                             <div className="TierRank">
-                                <span className="tierRank">Grandmaster</span>
+                                <span className="tierRank">
+                                    {changeTierSpell(this.state.RANKED_SOLO_5x5.tier)}&nbsp;
+                                    {
+                                        //不是王者宗师大师就显示小段位
+                                        ["CHALLENGER", "GRANDMASTER", "MASTER"].indexOf(this.state.RANKED_SOLO_5x5.tier) < 0
+                                            ? rankRoman2Int[this.state.RANKED_SOLO_5x5.rank]
+                                            : null
+                                    }
+                                </span>
                             </div>
                             <div className="TierInfo">
                               <span className="LeaguePoints">
-                                  {this.state.leaguePoints} LP
+                                  {this.state.RANKED_SOLO_5x5.leaguePoints} LP
                               </span>
                                 &nbsp;/&nbsp;
                                 <span className="WinLose">
-                                    <span className="wins">{this.state.wins}胜</span>
+                                    <span className="wins">{this.state.RANKED_SOLO_5x5.wins}胜</span>
                                     &nbsp;
-                                    <span className="losses">{this.state.losses}负</span>
+                                    <span className="losses">{this.state.RANKED_SOLO_5x5.losses}负</span>
                                     <br/>
-                                    <span className="winratio">胜率 {winRate}%</span>
+                                    <span className="winratio">胜率 {winSoloRate}%</span>
                                 </span>
                             </div>
                             <div className="LeagueName">
-                                {this.state.leagueName}
+                                {this.state.RANKED_SOLO_5x5.leagueName}
                             </div>
                         </div>
                     </div>
