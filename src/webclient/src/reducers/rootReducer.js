edf46e69ccf0543aae851daaa7430daf14bd4ce0
wmc3721 <512111127@qq.com>
2019-01-04 20:02:18 +0800

    rootReducer里的子reducer使用简写

--- src/webclient/src/reducers/rootReducer.js
+++ src/webclient/src/reducers/rootReducer.js
@@ -2,12 +2,12 @@
  *  将多个reducer合成一个大的 Reducer
  */
 import {combineReducers} from 'redux';
-import authReducer from './authReducer';
-import errorReducer from './errorReducer';
-import summonerReducer from './summonerReducer';
+import auth from './authReducer';
+import errors from './errorReducer';
+import summoner_detail from './summonerReducer';
 
 export default combineReducers({
-    auth: authReducer,
-    errors: errorReducer,
-    summoner_detail: summonerReducer,
+    auth,
+    errors,
+    summoner_detail,
 })
