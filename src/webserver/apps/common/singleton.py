a6b29eb27ecfbde112f4053fed0c30d2b4220fee
ubuntuRoot <512111127@qq.com>
2019-01-09 21:33:39 -0800

    注释无用打印

--- src/webserver/apps/common/singleton.py
+++ src/webserver/apps/common/singleton.py
@@ -5,10 +5,11 @@
 class Singleton(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '__instance'):
-            print('in new')
+            # print('in new')
             cls.__instance = object.__new__(cls, *args, **kwargs)
             cls.__instance.__singleton_init__(*args, **kwargs)
         return cls.__instance
 
     def __singleton_init__(self):
-        print("__Singleton_Init__")
+        # print("__Singleton_Init__")
+        pass
